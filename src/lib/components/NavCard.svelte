3fd2bcfb231249e28bcc393893e964941b80e658
Kyle Dampier <30601856+kyledampier@users.noreply.github.com>
2024-02-01 15:21:04 +0000

    nav layout almost done

--- src/lib/components/NavCard.svelte
+++ src/lib/components/NavCard.svelte
@@ -1,15 +1,13 @@
 <script lang="ts">
-	import { Hammer } from "lucide-svelte";
+    import Icon from "@iconify/svelte";
 
     export let title = "Title";
-    export let size = 64;
+    export let icon = "mdi:question-mark-circle-outline";
 </script>
 
-<div class="w-full aspect-square bg-surface-700 rounded-lg shadow-lg overflow-hidden">
-    <div class="aspect-content">
-        <div class="flex flex-col justify-center items-center p-4 text-center">
-            <Hammer size={size} />
+<div class="w-full aspect-square bg-surface-700 rounded-lg shadow-xl overflow-hidden hover:bg-surface-600 hover:scale-105 hover:outline outline-slate-700 transition-all">
+        <div class="flex flex-col justify-center items-center p-6 text-center h-full gap-4">
+            <Icon icon={icon} class="w-full h-full aspect-square p-4" />
             <h2 class="h3 font-bold">{title}</h2>
         </div>
-    </div>
 </div>
--- src/lib/components/SideBar.svelte
+++ src/lib/components/SideBar.svelte
@@ -1,11 +1,19 @@
 <script lang="ts">
     import NavCard from '$lib/components/NavCard.svelte';
-    import { Hammer } from 'lucide-svelte';
+
+    const MenuItems = [
+        { title: 'Nail It', icon: 'mdi:nail' },
+        { title: 'Fuse It', icon: 'mdi:shuffle' },
+        { title: 'Stabilize It', icon: 'ic:round-close-fullscreen' },
+        { title: 'Fix It', icon: 'mdi:hammer' },
+        { title: 'Compass It', icon: 'mdi:compass-outline'},
+        { title: 'Restore It', icon: 'mdi:restore' },
+        { title: "Torque It", icon: 'mdi:wrench'}
+    ];
 </script>
 
-<div class="flex flex-col w-72 p-6 gap-8">
-    <NavCard title="Nail It" />
-    <NavCard title="Fuse It" />
-    <NavCard title="Nail It" />
-    <NavCard title="Nail It" />
+<div class="flex flex-col w-64 p-6 gap-8">
+    {#each MenuItems as { title, icon }}
+        <NavCard {title} {icon} />
+    {/each}
 </div>
