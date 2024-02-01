81b4415e71054368a5638f2f3fb7be23e13bc89a
Kyle Dampier <30601856+kyledampier@users.noreply.github.com>
2024-02-01 15:29:36 +0000

    added three

--- src/lib/components/NavCard.svelte
+++ src/lib/components/NavCard.svelte
@@ -6,8 +6,8 @@
 </script>
 
 <div class="w-full aspect-square bg-surface-700 rounded-lg shadow-xl overflow-hidden hover:bg-surface-600 hover:scale-105 hover:outline outline-slate-700 transition-all">
-        <div class="flex flex-col justify-center items-center p-6 text-center h-full gap-4">
-            <Icon icon={icon} class="w-full h-full aspect-square p-4" />
-            <h2 class="h3 font-bold">{title}</h2>
-        </div>
+    <div class="flex flex-col justify-center items-center p-6 text-center h-full gap-4">
+        <Icon icon={icon} class="w-full h-full aspect-square p-4" />
+        <h2 class="h3 font-bold">{title}</h2>
+    </div>
 </div>
